3d65b26a229cfc0f6e468095c48bc80ba6775b1c
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-11-03 15:59:24 -0300

    relayed: improve auth modifier

--- packages/evm/contracts/core/modules/ControlledRelayable.sol
+++ packages/evm/contracts/core/modules/ControlledRelayable.sol
@@ -9,7 +9,12 @@ import "./Controlled.sol";
 contract ControlledRelayable is Controlled {
     string private constant ERROR_SENDER_NOT_ALLOWED = "CTD_SENDER_NOT_ALLOWED";
 
-    modifier authenticate(address _user) {
+    /**
+    * @dev Modifier for modules to support relayed transactions.
+    *      This modifier will check that the sender is the user acting on behalf of or a whitelisted relayer.
+    * @param _user Address of the user executing an action for
+    */
+    modifier authenticateSender(address _user) {
         require(_isSenderAllowed(_user), ERROR_SENDER_NOT_ALLOWED);
         _;
     }
--- packages/evm/contracts/test/core/modules/RelayedMock.sol
+++ packages/evm/contracts/test/core/modules/RelayedMock.sol
@@ -8,7 +8,7 @@ contract RelayedMock is ControlledRelayable {
 
     constructor(Controller _controller) ControlledRelayable(_controller) public {}
 
-    function authenticateCall(address _user) external authenticate(_user) {
+    function authenticateCall(address _user) external authenticateSender(_user) {
         emit Authenticated(_user, msg.sender);
     }
 
