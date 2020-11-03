efebccaa18490a4913901e055fbf4e67be15aff1
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-11-03 15:55:30 -0300

    evm: implement relayers whitelist

--- packages/evm/contracts/test/core/modules/RelayerMock.sol
+++ packages/evm/contracts/test/core/modules/RelayerMock.sol
@@ -1,7 +1,15 @@
 pragma solidity ^0.5.17;
 
-import "../../lib/TimeHelpersMock.sol";
-import "../../../core/modules/SignaturesValidatorRelayer.sol";
 
-
-contract RelayerMock is SignaturesValidatorRelayer, TimeHelpersMock {}
+contract RelayerMock {
+    function relay(address _target, bytes calldata _data) external {
+        bytes memory data = _data;
+        assembly {
+            let ptr := mload(0x40)
+            let result := call(gas, _target, 0, add(data, 0x20), mload(data), 0, 0)
+            returndatacopy(ptr, 0, returndatasize)
+            switch result case 0 { revert(ptr, returndatasize) }
+            default { return(ptr, returndatasize) }
+        }
+    }
+}
